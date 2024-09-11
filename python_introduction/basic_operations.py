aa9ca3f04b65723751a133830c3426890a9c6fbf
DessalegnChebude <letgo900@gmail.com>
2024-09-11 07:28:48 -0700

    Doing some basic arthemitic

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 6
+number2 = 9
+addition = number1 + number2
+difference = number2 - number1
+multiplication = number1 * number2
+print("Addition of 6 and 9 is ", addition)
+print("Subtraction of 9 and 6 ", difference)
+print("Multiplication of 6 and 9 is ", multiplication)
