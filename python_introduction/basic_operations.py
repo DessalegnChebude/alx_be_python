001373f6de184115bba219cb0fea272b6642cffd
DessalegnChebude <letgo900@gmail.com>
2024-09-11 07:41:02 -0700

    sum bug fix on value assignment

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,8 @@
-number1 = 6
-number2 = 9
-addition = number1 + number2
+number1 = 5
+number2 = 10
+addition = number2 + number1
 difference = number2 - number1
-multiplication = number1 * number2
-print("Addition of 6 and 9 is ", addition)
-print("Subtraction of 9 and 6 ", difference)
-print("Multiplication of 6 and 9 is ", multiplication)
+multiplication = number2 * number1
+print("Addition of 10 and 5 is ", addition)
+print("Subtraction of 10 and 5 ", difference)
+print("Multiplication of 10 and 5 is ", multiplication)
