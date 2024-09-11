10155d1c077549b3825493065b31ca34c0158966
DessalegnChebude <letgo900@gmail.com>
2024-09-11 07:46:21 -0700

    Doing some arthemitic operation

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,7 +1,7 @@
-number1 = 5
-number2 = 10
+number1 = 10
+number2 = 5
 addition = number2 + number1
-difference = number2 - number1
+difference = number1 - number2
 multiplication = number2 * number1
 print("Addition of 10 and 5 is ", addition)
 print("Subtraction of 10 and 5 ", difference)
