3a5220c25d0987a2f973834e04702053390f3f14
DessalegnChebude <letgo900@gmail.com>
2024-09-29 12:21:00 +0300

    temprature conversion app

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,33 +1,31 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
-while True:
+
+def convert_to_celsius(fahrenheit):
+    return (fahrenheit-32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+def convert_to_fahrenheit(celsius):
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+
+def main():
     try:
-        temprature = float(input("Enter the temperature to convert: "))
+        temprature = float(input("Enter the temprature in number: "))
         unit_measure = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").lower()
 
-        if unit_measure == "f":
-            def convert_to_celsius(fahrenheit):
-                celcius = (fahrenheit-32) * FAHRENHEIT_TO_CELSIUS_FACTOR
-                print(f"{fahrenheit}°F is {celcius}°C")
-
-                # °C = (32°F − 32) × 5/9
-            convert_to_celsius(temprature)
-            # print(temp_in_celcius)
-        elif unit_measure == "c":
-            def convert_to_fahrenheit(celsius):
-                fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
-                print(f"{celsius}°C is {fahrenheit}°F")
-                # 32°C × 9/5) + 32
-
-            convert_to_fahrenheit(temprature)
-        elif unit_measure == "exit":
-            print("Goodbye!!!!")
-            break
-        else: 
-            print("Invalid Input pls ")
-
+        if unit_measure == "c":
+            temp_celsius = convert_to_fahrenheit(temprature)
+            print(f"{temprature}°C is {temp_celsius}°F")
+        elif unit_measure == "f":
+            temp_fahrenheit = (convert_to_celsius(temprature))
+            print(f"{temprature}°F is {temp_fahrenheit}°C")
+        
+        else:
+            print("Invalid Input choice pls ")
+        
     except ValueError:
         print("Invalid temperature. Please enter a numeric value.")
-    
 
+
+if __name__ == "__main__":
+    main()
 
