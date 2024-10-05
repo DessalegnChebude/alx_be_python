a76947bf84fe2b2f299c3153948105927731cb50
DessalegnChebude <letgo900@gmail.com>
2024-10-06 00:57:50 +0300

    oop

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -8,10 +8,9 @@ class BankAccount:
 
     def withdraw(self, amount):
         if self.account_balance > amount:
-            # self.account_balance -= amount
+            self.account_balance -= amount
             return True
         else:
-            print("Insufficient funds.")
             return False
 
     def display_balance(self):
