f3c0dd7172233665de462f29e63431f6c4bb4864
DessalegnChebude <letgo900@gmail.com>
2024-10-05 23:41:32 +0300

    oop bank logics practice

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount():
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+
+    def deposit(amount):
+        self.account_balance += amount
+        return self.account_balance
+
+    def withdraw(amount):
+        if self.account_balance > amount:
+            return True
+        else:
+            return False
+
+    def display_balance():
+        print(f"The current balance is: ${self.account_balance}")
