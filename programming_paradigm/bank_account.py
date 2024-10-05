73cb1888e4337d58f4e602c0f9d9842123e15f08
DessalegnChebude <letgo900@gmail.com>
2024-10-06 00:52:17 +0300

    oop

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,15 +3,12 @@ class BankAccount:
         self.account_balance = account_balance = 0
 
     def deposit(self, amount):
-        self.account_balance += amount
+        # self.account_balance += amount
         return self.account_balance
 
     def withdraw(self, amount):
-        if amount <= 0:
-            print("Withdrawal amount must be positive.")
-            return False
         if self.account_balance > amount:
-            self.account_balance -= amount
+            # self.account_balance -= amount
             return True
         else:
             print("Insufficient funds.")
